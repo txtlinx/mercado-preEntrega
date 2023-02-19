10f77a9219a52403bee6e94c1fbb356742ae6077
Pablo <ti.dosantos.pablo@gmail.com>
2023-02-18 21:10:14 -0300

    #styled-components

--- src/Component/pages/Home.js
+++ src/Component/pages/Home.js
@@ -1,11 +1,34 @@
+import {
+  ConstButtonStyleHereda,
+  TomatoButton,
+  ConstButtonStylePrimary,
+} from "./styles";
+import { Thing } from "../../Component/layaout/styles.jsx";
 function Home() {
-  return (<>
+  return (
+    <>
       <h1>
-        Bienvenido al Home
+        <TomatoButton as="a" href="#">
+          Bienvenido al Home - Link button
+        </TomatoButton>
+        <TomatoButton inputColor="green">Hola h1</TomatoButton>
+        <TomatoButton>Button Tomato</TomatoButton>
+       
+        <ConstButtonStylePrimary>NORMAL</ConstButtonStylePrimary>
+        <ConstButtonStylePrimary>Heredado normal</ConstButtonStylePrimary>
+        <ConstButtonStyleHereda Primary>
+          Primary Heredado
+        </ConstButtonStyleHereda>
+
+        <Thing>Prueba1</Thing>
+        <Thing className="something">Prueba1</Thing>
+        <Thing className="something-else">Prueba1</Thing>
+        
       </h1>
+      
       <p>loremp ipsu</p>
- </>
-  )
+    </>
+  );
 }
 
-export default Home
+export default Home;
